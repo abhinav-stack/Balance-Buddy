0054f4fe9f802b6242c33e51bc41e4b1a80b784b
abhinav-stack <72855781+abhinav-stack@users.noreply.github.com>
2025-01-10 00:00:54 +0530

    [FIX] some error regarding vite

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     auth: {
-        domain: 'bfnZZJtV5vHVq0F2FfXZtCEJBSRMgkt6',
-        clientId: 'dev-6nc2z1t2uxcsie23.us.auth0.com'
+        domain: 'dev-6nc2z1t2uxcsie23.us.auth0.com',
+        clientId: 'bfnZZJtV5vHVq0F2FfXZtCEJBSRMgkt6'
     }
 }
